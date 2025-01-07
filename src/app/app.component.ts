8030756bb708dd022a0944099c0597702fcc40bf
Arnaud BODEL <53226044+ArnaudBDL@users.noreply.github.com>
2025-01-07 11:46:21 +0100

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../../environments/environment.dev';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   public title:string = 'Click Counter';
+  public counter: number = 0;
+  private limit: number = environment.counterLimit;
+
+  less(): void {
+    if (this.counter <= this.limit*-1) {
+      return;
+    }
+
+    this.counter--;
+  }
+
+  more(): void {
+    if (this.counter >= this.limit) {
+      return;
+    }
+
+    this.counter++;
+  }
 }
